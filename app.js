86f8be5059383411219f49083bd5ba5c988f39fa
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 19:45:41 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // write code here
+function checkAge(user) {
+  const { firstName, lastName, age } = user;
+
+  if (age > 18) {
+    console.log(`Hello, ${firstName} ${lastName}!`);
+  } else {
+    console.log(`Access denied: You are not eligible to use this service.`);
+  }
+}
+
+const firstNameInput = prompt("Enter your first name:");
+const lastNameInput = prompt("Enter your last name:");
+const ageInput = prompt("Enter your age:");
+
+const user = {
+  firstName: firstNameInput,
+  lastName: lastNameInput,
+  age: parseInt(ageInput) 
+};
+
+checkAge(user);
